a994c25d6cbdf0f19003abdc11407ce158310f5a
33Frok1e <mratikrishna@gmail.com>
2025-02-01 11:21:06 +0530

    db changes

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://ratikrishna:Rati%402000@cluster1.kj3wm.mongodb.net/todo");
+mongoose.connect("MongoURL");
+// mongodb+srv://ratikrishna:Rati%402000@cluster1.kj3wm.mongodb.net/todo
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
